2d0c7d59567c3b41b16d8a346cd0d224d4ed4b78
krtwgl <krtwgl@gmail.com>
2026-08-01 15:32:33 +0530

    created swiggy-top3-recom using cookiecutter

---
+++ swiggy-top3-recco/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project whil help recommend the top 3 food items on swiggy homepage.',
+    author='K Ravi Teja',
+    license='',
+)
